a0141ec361ae46df239cef7e369f6971340135d7
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-09-09 16:54:33 +0100

    Robustness to variation in .md5 format

--- scripts/check-md5.sh
+++ scripts/check-md5.sh
@@ -15,7 +15,7 @@ for d in "$@"; do
 	# assume that stripping the suffix ".md5" gives the file name that
 	# the md5sum is for
 	new=`md5sum < ${f%.md5} | perl -pe 's/ +- *$//'`
-	old=`cat $f | perl -pe 's/^MD5 \(.*?\) = //'`
+	old=`cat $f | perl -pe 's/^MD5\s*\(.*?\)\s*=\s*//'`
 	if [[ "$new" != "$old" ]]; then
 	    echo "Mismatch: $f: $new vs $old"
 	fi
