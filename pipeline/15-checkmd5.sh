189551654931291b39322710716daea1bc113bbe
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-09-10 12:07:28 +0100

    Don't re-check md5sum

--- pipeline/15-checkmd5.sh
+++ pipeline/15-checkmd5.sh
@@ -3,20 +3,32 @@
 # Verify md5 checksums.
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTNAME=$(basename "$0")
+
 DATADIR="$SCRIPTDIR/../data/original-data"
+STATUSDIR="$SCRIPTDIR/../data/status"
 
 set -eu
+
+mkdir -p "$STATUSDIR"
 
 for f in "$DATADIR"/*.md5; do
     # stripping ".md5" gives name of file that md5sum is for
     b="${f%.md5}"
-    ref=$(cat "$f" | perl -pe 's/^MD5\s*\(.*?\)\s*=\s*//')
-    val=$(md5sum < "$b" | perl -pe 's/ +- *$//')
-    echo -n "Checking md5sum $f for $b ... " >&2
-    if [[ "$ref" != "$val" ]]; then
-	echo $'\n'"ERROR: md5sum mismatch for $b: $ref vs $val" >&2
-	exit 1
+    s="$STATUSDIR"/$(basename "$f" .md5).log
+    if [ -e "$s" ] && [ "$s" -nt "$f" ] && \
+	   grep -qF "Done $SCRIPTNAME" "$s"; then
+	echo "$SCRIPTNAME done for $b ($s), skipping ..." >&2
     else
-	echo "OK" >&2
+	ref=$(cat "$f" | perl -pe 's/^MD5\s*\(.*?\)\s*=\s*//')
+	val=$(md5sum < "$b" | perl -pe 's/ +- *$//')
+	echo -n "Checking md5sum $f for $b ... " >&2
+	if [[ "$ref" != "$val" ]]; then
+	    echo $'\n'"ERROR: md5sum mismatch for $b: $ref vs $val" >&2
+	    exit 1
+	else
+	    echo "OK" >&2
+	fi
+	echo "Done $SCRIPTNAME" > "$s"
     fi
 done
